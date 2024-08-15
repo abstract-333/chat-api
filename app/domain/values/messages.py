dced6b24699ba33e427401c9fecc161a1838dec7
bashar <basharhasan74@gmail.com>
2024-08-15 16:30:21 +0300

    feature(value): add Title

--- app/domain/values/messages.py
+++ app/domain/values/messages.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from typing import Any, TypeVar
 
-from domain.exceptions.messages import TextTooLongException
+from domain.exceptions.messages import EmptyTextError, TextTooLongException
 from domain.values.base import BaseValueObject
 
 VT = TypeVar(name="VT", bound=Any)
@@ -12,6 +12,21 @@ class Text(BaseValueObject):
     value: str
 
     def validate(self) -> None:
+        if not self.value:
+            raise EmptyTextError()
+
+    def as_generic_type(self) -> str:
+        return str(object=self.value)
+
+
+@dataclass(frozen=True)
+class Title(BaseValueObject):
+    value: str
+
+    def validate(self) -> None:
+        if not self.value:
+            raise EmptyTextError()
+
         if len(self.value) > 255:
             raise TextTooLongException(text=self.value)
 
