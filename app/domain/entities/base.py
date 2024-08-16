27b8569ee5dd6415bc3b6d1a04aafea91dbd5225
bashar <basharhasan74@gmail.com>
2024-08-16 04:07:12 +0300

    feature(entity): add _events and implement register_event and pull_events

--- app/domain/entities/base.py
+++ app/domain/entities/base.py
@@ -1,7 +1,10 @@
 from abc import ABC
+from copy import copy
 from dataclasses import dataclass, field
 from datetime import datetime
 
+from events.base import BaseEvent
+from events.messages import NewMessageReceivedEvent
 from utils.uuid import get_uuid4
 
 
@@ -15,6 +18,18 @@ class BaseEntity(ABC):
         default_factory=datetime.now,
         kw_only=True,
     )
+    _events: list[BaseEvent] = field(
+        default_factory=list,
+        kw_only=True,
+    )
+
+    def register_event(self, event: BaseEvent) -> None:
+        self._events.append(event)
+
+    def pull_events(self) -> list[BaseEvent]:
+        registered_events: list[BaseEvent] = copy(self._events)
+        self._events.clear()
+        return registered_events
 
     def __hash__(self) -> int:
         return hash(self.oid)
