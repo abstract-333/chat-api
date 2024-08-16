d36d71af98d779c5eddcc820383e7a80b48923e8
bashar <basharhasan74@gmail.com>
2024-08-16 04:08:03 +0300

    test(values): add test_new_message_events

--- app/tests/values/test_messages.py
+++ app/tests/values/test_messages.py
@@ -5,6 +5,8 @@ import pytest
 from domain.entities.messages import Chat, Message
 from domain.exceptions.messages import EmptyTextError, TextTooLongException
 from domain.values.messages import Text, Title
+from events.base import BaseEvent
+from events.messages import NewMessageReceivedEvent
 
 
 @pytest.mark.parametrize(
@@ -69,3 +71,36 @@ def test_add_message_to_chat(
         assert message in chat.messages
         chat.add_message(message=message)
         assert message in chat.messages
+
+
+@pytest.mark.parametrize(
+    argnames="message, title ,expectation",
+    argvalues=[
+        (
+            Message(Text("Hello World Chat")),
+            Title("Hello World Title"),
+            does_not_raise(),
+        ),
+    ],
+)
+def test_new_message_events(
+    message: Message,
+    title: Title,
+    expectation: Any,
+):
+    with expectation:
+        chat = Chat(title=title)
+        chat.add_message(message=message)
+        assert message in chat.messages
+
+        events = chat.pull_events()
+        pulled_events = chat.pull_events()
+        assert not pulled_events, pulled_events
+        assert len(events) == 1, events
+
+        new_event = events[0]
+
+        assert isinstance(new_event, NewMessageReceivedEvent), new_event
+        assert new_event.message_oid == message.oid
+        assert new_event.chat_oid == chat.oid
+        assert new_event.message_text == message.text.as_generic_type()
