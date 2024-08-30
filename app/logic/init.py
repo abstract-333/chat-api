4911ea2aee00eaf1545fefb701ec1eeb7986f705
bashar <basharhasan74@gmail.com>
2024-08-30 03:40:40 +0300

    feature(init): add init file for DI

--- app/logic/init.py
+++ app/logic/init.py
@@ -1,13 +1,36 @@
+from functools import lru_cache
+from punq import Container, Scope
+
+
 from infra.repositories.base import BaseChatRepository
-from logic.commands.messages import CreateChatCommandHandler, CreateChatCommand
+from infra.repositories.memory import MemoryChatRepository
+from logic.commands.messages import CreateChatCommand, CreateChatCommandHandler
 from logic.mediator import Mediator
 
 
-def init_mediator(
-    mediator: Mediator,
-    chat_repository: BaseChatRepository,
-):
-    mediator.register_command(
-        command=CreateChatCommand,
-        command_handlers=[CreateChatCommandHandler(chat_repository=chat_repository)],
+@lru_cache(1)
+def init_container() -> Container:
+    return _init_container()
+
+
+def _init_container() -> Container:
+    container = Container()
+    container.register(
+        service=BaseChatRepository,
+        factory=MemoryChatRepository,
+        scope=Scope.singleton,
     )
+    container.register(CreateChatCommandHandler)
+
+    def init_mediator() -> Mediator:
+        mediator = Mediator()
+        mediator.register_command(
+            command=CreateChatCommand,
+            command_handlers=[container.resolve(service_key=CreateChatCommandHandler)],
+        )
+
+        return mediator
+
+    container.register(service=Mediator, factory=init_mediator)
+
+    return container
