d3ff43aedc49d7504da0bf13e1a42fff2f2e9d3c
bashar <basharhasan74@gmail.com>
2024-08-30 03:39:25 +0300

    refactor(values): add return type

--- app/domain/values/base.py
+++ app/domain/values/base.py
@@ -11,7 +11,7 @@ VT = TypeVar("VT", bound=Any)
 class BaseValueObject(ABC, Generic[VT]):
     value: VT
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
         self.validate()
 
     @abstractmethod
