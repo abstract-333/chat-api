c46df877ff4b58ed6d5b3038aed627dee497fb1b
bashar <basharhasan74@gmail.com>
2024-10-14 17:56:41 +0300

    test(api): test empty chat and too long chat variants

--- app/tests/application/api/test_messages.py
+++ app/tests/application/api/test_messages.py
@@ -1,5 +1,5 @@
 from faker import Faker
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from fastapi.testclient import TestClient
 from httpx import Response
 import pytest
@@ -17,3 +17,36 @@ async def test_create_chat_success(
     assert response.is_success
     json_data = response.json()
     assert json_data["title"] == title
+
+
+@pytest.mark.asyncio
+async def test_create_chat_fail_text_too_long(
+    app: FastAPI,
+    client: TestClient,
+    faker: Faker,
+) -> None:
+    url = app.url_path_for("create_chat_handler")
+    title: str = faker.text(max_nb_chars=500)
+
+    response: Response = client.post(url=url, json={"title": title})
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST, response.json()
+    json_data = response.json()
+
+    assert json_data["detail"]["error"]
+
+
+@pytest.mark.asyncio
+async def test_create_chat_fail_text_empty(
+    app: FastAPI,
+    client: TestClient,
+    faker: Faker,
+) -> None:
+    url = app.url_path_for("create_chat_handler")
+
+    response: Response = client.post(url=url, json={"title": ""})
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST, response.json()
+    json_data = response.json()
+
+    assert json_data["detail"]["error"]
