25c8b053fe380811a94521390df2becac07bdef0
bashar <basharhasan74@gmail.com>
2024-11-28 16:16:58 +0300

    ci: fix .env conflicts

--- app/settings/config.py
+++ app/settings/config.py
@@ -11,5 +11,6 @@ class Config(BaseSettings):
     mongodb_connection_uri: str = Field(alias="MONGO_DB_CONNECTION_URI")
     mongodb_chat_database: str = Field(default="chat", alias="MONGODB_CHAT_DATABASE")
     mongodb_chat_collection: str = Field(
-        default="chat", alias="MONGODB_CHAT_COLLECTION",
+        default="chat",
+        alias="MONGODB_CHAT_COLLECTION",
     )
