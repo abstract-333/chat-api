5d152f33fb868ef00d839c0b6706367284edbe7b
bashar <basharhasan74@gmail.com>
2024-11-28 15:30:39 +0300

    style: add MessageDocument and ChatDocument TypeDicts for typing

--- app/infra/repositories/convertors.py
+++ app/infra/repositories/convertors.py
@@ -1,18 +1,32 @@
-from typing import (
-    Any,
-    Mapping,
-)
+import datetime
+from typing import TypedDict
 
 from domain.entities.messages import (
     Chat,
     Message,
 )
+from domain.values.messages import (
+    Text,
+    Title,
+)
 
 
-# TODO: add typing
+class MessageDocument(TypedDict):
+    oid: str
+    text: str
+    created_at: datetime.datetime
+
+
+class ChatDocument(TypedDict):
+    oid: str
+    title: str
+    created_at: datetime.datetime
+    messages: list[MessageDocument]
+
+
 def convert_message_entity_to_document(
     message: Message,
-) -> dict[str, Any]:
+) -> MessageDocument:
     return {
         "oid": message.oid,
         "created_at": message.created_at,
@@ -21,16 +35,16 @@ def convert_message_entity_to_document(
 
 
 def convert_message_document_to_entity(
-    document: Mapping[str, Any],
+    document: MessageDocument,
 ) -> Message:
     return Message(
         oid=document["oid"],
-        text=document["text"],
+        text=Text(document["text"]),
         created_at=document["created_at"],
     )
 
 
-def convert_chat_entity_to_document(chat: Chat) -> dict[str, Any]:
+def convert_chat_entity_to_document(chat: Chat) -> ChatDocument:
 
     return {
         "oid": chat.oid,
@@ -42,10 +56,10 @@ def convert_chat_entity_to_document(chat: Chat) -> dict[str, Any]:
     }
 
 
-def convert_chat_document_to_entity(document: Mapping[str, Any]) -> Chat:
+def convert_chat_document_to_entity(document: ChatDocument) -> Chat:
     return Chat(
         oid=document["oid"],
-        title=document["title"],
+        title=Title(document["title"]),
         created_at=document["created_at"],
         messages={
             convert_message_document_to_entity(message)
