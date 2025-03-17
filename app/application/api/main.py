fd65e360688453b18e80cdc389f36166d0e2e8c4
bashar <basharhasan74@gmail.com>
2025-03-17 16:10:31 +0300

    fix: nullable values url

--- app/application/api/main.py
+++ app/application/api/main.py
@@ -13,8 +13,9 @@ def create_app() -> FastAPI:
     def register_static_docs_routes(app: FastAPI) -> None:
         @app.get(path="/api/docs", include_in_schema=False)
         async def custom_swagger_ui_html() -> HTMLResponse:
+            openapi_url: str = app.openapi_url if app.openapi_url else ""
             return get_swagger_ui_html(
-                openapi_url=app.openapi_url,
+                openapi_url=openapi_url,
                 title=app.title + " - Swagger UI",
                 oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
                 swagger_js_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js",
@@ -27,8 +28,9 @@ def create_app() -> FastAPI:
 
         @app.get("/api/redoc", include_in_schema=False)
         async def redoc_html() -> HTMLResponse:
+            openapi_url: str = app.openapi_url if app.openapi_url else ""
             return get_redoc_html(
-                openapi_url=app.openapi_url,
+                openapi_url=openapi_url,
                 title=app.title + " - ReDoc",
                 redoc_js_url="https://unpkg.com/redoc@next/bundles/redoc.standalone.js",
             )
