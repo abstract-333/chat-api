a5456ec104c46b2d5bcc2dfaa9cc8c939d000fb2
bashar <basharhasan74@gmail.com>
2025-03-20 21:13:00 +0300

    feat: make prod and dev depend code

--- app/application/api/main.py
+++ app/application/api/main.py
@@ -3,12 +3,14 @@ from fastapi import FastAPI
 from application.api.static_docs import register_static_docs_routes
 from application.api.v1.health.handlers import router as health_router_v1
 from application.api.v1.messages.handlers import router as message_router_v1
+from logic.init import init_container
+from settings.config import Config
 
 
 def create_app() -> FastAPI:
     fastapi_app = FastAPI(
         title="Chat API",
-        debug=True,
+        debug=init_container().resolve(Config).debug,
         docs_url=None,
         redoc_url=None,
     )
--- app/settings/config.py
+++ app/settings/config.py
@@ -9,8 +9,12 @@ class Config(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
     mongodb_connection_uri: str = Field(alias="MONGO_DB_CONNECTION_URI")
-    mongodb_chat_database: str = Field(default="chat", alias="MONGODB_CHAT_DATABASE")
+    mongodb_chat_database: str = Field(default="chat", alias="MONGO_DB_CHAT_DATABASE")
     mongodb_chat_collection: str = Field(
         default="chat",
-        alias="MONGODB_CHAT_COLLECTION",
+        alias="MONGO_DB_CHAT_COLLECTION",
     )
+
+    debug: bool = Field(alias="DEBUG", default=True)
+    worker: str = Field(alias="WORKER")
+    workers_config: str = Field(alias="WORKERS_CONFIG")
