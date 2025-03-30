1172a046e7a80c34b7bab5a44a336c2ce63f7bab
Bashar Hasan <basharhasan74@gmail.com>
2025-03-30 23:21:14 +0300

    add user group for better security in Dockerfile

--- app/tests/application/api/test_messages.py
+++ app/tests/application/api/test_messages.py
@@ -53,3 +53,30 @@ async def test_create_chat_fail_text_empty(
     json_data = response.json()
 
     assert json_data["detail"]["error"]
+
+
+# @pytest.mark.asyncio
+# async def test_create_message_success(
+#     app: FastAPI,
+#     client: TestClient,
+#     faker: Faker,
+#     chats_repository: BaseChatsRepository,
+# ) -> None:
+#     url = app.url_path_for("create_chat_handler")
+#     title: str = faker.text(max_nb_chars=30)
+#     response: Response = client.post(url=url, json={"title": title})
+#     json_data = response.json()
+#     chat_oid: str = json_data["oid"]
+#     print(json_data["oid"])
+#
+#     assert await chats_repository.check_chat_exists_by_title(
+#         title=title,
+#     )
+#     # TODO: Fix 404 chat not found, ever it was created...
+#
+#     url = app.url_path_for("create_message_handler", chat_oid=chat_oid)
+#     message_text: str = faker.text(max_nb_chars=33)
+#     response: Response = client.post(url=url, json={"text": message_text})
+#     assert response.is_success
+#     json_data = response.json()
+#     assert json_data["text"] == message_text
--- dev.Dockerfile
+++ dev.Dockerfile
@@ -21,8 +21,14 @@ RUN --mount=type=cache,target=/root/.cache/uv \
 # will fail.
 FROM python:3.13-slim-bookworm@sha256:8f3aba466a471c0ab903dbd7cb979abd4bda370b04789d25440cc90372b50e04
 
+# Create user and group 'app'
+RUN groupadd -r app && useradd -r -g app app
+
 # Copy the application from the builder
 COPY --from=builder --chown=app:app /app /app
+
+# app should run as user app, not root for better security
+USER app
 
 # Place executables in the environment at the front of the path
 ENV PATH="/app/.venv/bin:$PATH"
